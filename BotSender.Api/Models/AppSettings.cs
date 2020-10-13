627bf44c3cd02a799f32696d16312d0bcbff1d48
18368262 <18368262@CAB-WSN-0014373>
2020-10-13 19:13:16 +0300

    3

--- BotSender.Api/Models/AppSettings.cs
+++ BotSender.Api/Models/AppSettings.cs
@@ -2,7 +2,7 @@
 {
     public static class AppSettings
     {
-        public static string Url { get; set; } = "https://sendertgbot.herokuapp.com/{0}";
+        public static string Url { get; set; } = "https://sendertgbot.herokuapp.com/";
         public static string Name { get; set; } = "user_message_sender_bot";
         public static string Key { get; set; } = "1134203526:AAG7R2jA2HSMoXmD7cF9jfPyaIWMhEH8lbI";
     }
--- BotSender.Api/Models/Bot.cs
+++ BotSender.Api/Models/Bot.cs
@@ -23,10 +23,37 @@ namespace BotSender.Api.Models
             Commands.Append(new HelloCommand());
 
             client = new TelegramBotClient(AppSettings.Key);
-            var hook = string.Format(AppSettings.Url, "api/message/update");
+            var hook = AppSettings.Url + "api/message/update";
             await client.SetWebhookAsync(hook);
 
             return client;
+        }
+
+        public static async Task HandleUpdatesAsync()
+        {
+            while(true)
+            {
+                await Task.Delay(500);
+                var c = await Get();
+                var updates = await c.GetUpdatesAsync(0, 100, 0);
+                foreach (var update in updates)
+                {
+                    if (update == null) continue;
+
+                    var commands = Commands;
+                    var message = update.Message;
+
+                    foreach (var command in commands)
+                    {
+                        if (command.Contains(message.Text))
+                        {
+                            await command.Execute(message, c);
+                            break;
+                        }
+                    }
+                }
+            }
+            
         }
     }
 }
