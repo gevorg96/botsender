f52c4905c10fec0512e7af225ac1590492eefce3
18368262 <18368262@CAB-WSN-0014373>
2020-10-19 16:04:50 +0300

    2

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var TelegramBot = require('node-telegram-bot-api');
+var express = require('express');
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Bot works.')
+})
+
+app.listen(3000, () => {
+  console.log(`App listening...`)
+})
+
 var token = '1134203526:AAG7R2jA2HSMoXmD7cF9jfPyaIWMhEH8lbI';
 
 var bot = new TelegramBot(token, { polling: true });
 
 bot.onText(/command1/, function (msg, match) {
-    var fromId = msg.from.id; // Получаем ID отправителя
+    var fromId = msg.from.id; 
     bot.sendMessage(fromId, "Тест пройден!!!");
 });
