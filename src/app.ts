46ac3261b45497fb67dcb85282192a42ac3d0d75
gevkes <18368262@CAB-WSN-0014373>
2020-10-23 13:49:33 +0300

    3

--- src/app.ts
+++ src/app.ts
@@ -7,16 +7,16 @@ const app: Application = express();
 const bot = new TelegramBot('1134203526:AAG7R2jA2HSMoXmD7cF9jfPyaIWMhEH8lbI', { polling: true });
 
 const sendMessage = (message: IMessage) => {
-  var fromId = message.from.id;
+  var fromId = message.from.id
   bot.sendMessage(fromId, MessageRoute.messages[message.text])
-}
+};
 
 MessageRoute.routes.forEach(x => bot.onText(x, (msg : IMessage, match : IMatch) => sendMessage(msg)))
 
 app.get('/', (req : Request, res : Response) => {
   res.send('Bot works.')
-})
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`App listening...`)
-})
+});
